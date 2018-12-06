8b295ef28f89167b8800fbc21e930ecb1e7ae82b
yongjunya <1172676590@qq.com>
2018-12-06 12:32:03 +0800

    内容部分完成

---
+++ components/content/content.js
@@ -0,0 +1,28 @@
+// components/content/content.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    images: {
+      type: String
+    },
+    content: {
+      type:String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
