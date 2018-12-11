617c12f7ebc47c7c79db69f41b20460639716e01
心心 <1634445003@qq.com>
2018-12-11 12:44:54 +0800

    修改

--- components/index/share/share.js
+++ components/index/share/share.js
@@ -18,6 +18,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+      music:function(){
+             const back = wx.getBackgroundAudioManager();
+        back.src = 'http://music.163.com/song/media/outer/url?id=29719651.mp3';
+        back.title = '你是一只猪';
+        back.coverImgUrl="./image/3.png";
+        back.play();
+        back.onPlay(() => {
+          console.log('猪跑了')
+        })
+        back.onEnded(() => {
+          console.log('猪回来了')
+        })
+      }
   }
 })
